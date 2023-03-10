1968c5687c9271857483d26ee0d5e5c024a470eb
Dmitriy K <d.komerzan@gmail.com>
2023-03-10 16:49:21 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ const App = ({ signOut }) => {
         </Flex>
       ))}
       </View> */}
-      <Button onClick={signOut}>Sign Out (test )</Button>
+      <Button onClick={signOut}>Sign Out (test123 )</Button>
     </View>
   );
 };
